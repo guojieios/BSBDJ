be33e9798b8564720f1f9067e751008a386d1928
guojieios <531829791@qq.com>
2017-04-11 13:46:39 +0800

    创建项目
    创建新的项目

---
+++ BSBDJ/BSBDJ/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BSBDJ
+//
+//  Created by Jerry_hqyj on 17/4/11.
+//  Copyright © 2017年 Jerry_hqyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BSBDJ/BSBDJ/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BSBDJ
+//
+//  Created by Jerry_hqyj on 17/4/11.
+//  Copyright © 2017年 Jerry_hqyj. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
